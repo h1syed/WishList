518a02111043208b171242ff834ab5813e75959c
EC2 Default User <ec2-user@ip-172-31-43-130.ec2.internal>
2020-03-11 18:57:55 +0000

    adding user object, website bucket policy, and yml file

---
+++ app/service/src/main/java/com/wishlist/WishListApplication.java
@@ -0,0 +1,12 @@
+package com.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListApplication.class, args);
+    }
+}
